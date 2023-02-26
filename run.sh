47b8feba06baee4fb65ab27c8239adda6a96a170
“kengerlwl” <kengerlwl@qq.com>
2023-02-26 23:58:18 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 hexo clean
 hexo g
-hexo d
-git add -A
-git commit -m "$1"
-git push origin master
+cd public
+
+git init
+
+git config --global user.name “kengerlwl”
+git config --global user.email "kengerlwl@qq.com"
+
+
+git add .
+git commit -m "$(date) Update from Action"
+
+
+git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
