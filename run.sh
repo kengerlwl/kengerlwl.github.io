f91016ae5d0be2daa8042838e674329739af923f
“kengerlwl” <kengerlwl@qq.com>
2023-02-27 00:01:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,20 @@ hexo clean
 hexo g
 cd public
 
+# 初始化
 git init
 
-git config --global user.name “kengerlwl”
-git config --global user.email "kengerlwl@qq.com"
+# 设置账号
+git config user.name "name"
+git config user.email "email"
 
-
+# commit
 git add .
 git commit -m "$(date) Update from Action"
 
+# 本地切换分支
+git branch gh-pages 
+git checkout gh-pages 
 
+# 强制上传
 git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
