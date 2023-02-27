0c4e16a71ef585c810f793729a65a78ea0a9f9ec
“kengerlwl” <kengerlwl@qq.com>
2023-02-27 09:06:19 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-hexo clean
-hexo g
 cd public
 
 
@@ -10,13 +8,22 @@ git init
 git config user.name "name"
 git config user.email "email"
 
-# commit
-git add .
-git commit -m "$(date) Update from Action"
 
 # 本地切换分支
 git branch gh-pages 
 git checkout gh-pages 
+
+
+# 生成静态页面
+hexo clean
+hexo g
+
+
+# commit
+git add .
+git commit -m "$(date) Update from Action"
+
+
 
 # 强制上传
 git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
