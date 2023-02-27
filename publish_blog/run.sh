bae35db97669590b781378239fc7c37ffc13e6d2
“kengerlwl” <kengerlwl@qq.com>
2023-02-27 19:56:45 +0800

    update

--- publish_blog/run.sh
+++ publish_blog/run.sh
@@ -9,20 +9,20 @@ hexo g
 cd public
 
 
-# # 初始化
-# git init
+# 初始化
+git init
 
-# # 设置账号
-# git config user.name "name"
-# git config user.email "email"
+# 设置账号
+git config user.name "name"
+git config user.email "email"
 
-# # commit
-# git add .
-# git commit -m "$(date) Update from Action"
+# commit
+git add .
+git commit -m "$(date) Update from Action"
 
-# # 本地切换分支
-# git branch gh-pages 
-# git checkout gh-pages 
+# 本地切换分支
+git branch gh-pages 
+git checkout gh-pages 
 
-# # 强制上传
-# git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
+# 强制上传
+git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
