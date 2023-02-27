c005492bf6cd94eb6ca59efd0b321abe2fa29be7
“kengerlwl” <kengerlwl@qq.com>
2023-02-27 17:11:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,30 +1,23 @@
 # 生成静态页面
-
-node -v
-hexo -v
-
 hexo clean
 hexo g
 cd public
 
 
-echo 查看前端页面
-cat index.html
+# 初始化
+git init
 
-# # 初始化
-# git init
+# 设置账号
+git config user.name "name"
+git config user.email "email"
 
-# # 设置账号
-# git config user.name "name"
-# git config user.email "email"
+# commit
+git add .
+git commit -m "$(date) Update from Action"
 
-# # commit
-# git add .
-# git commit -m "$(date) Update from Action"
+# 本地切换分支
+git branch gh-pages 
+git checkout gh-pages 
 
-# # 本地切换分支
-# git branch gh-pages 
-# git checkout gh-pages 
-
-# # 强制上传
-# git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
+# 强制上传
+git push --force --quiet "https://kengerlwl:${GITHUB_TOKEN}@github.com/kengerlwl/kengerlwl.github.io.git"  gh-pages    
